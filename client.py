6aa432c272ed7a6cfdc3b0f2c48eb0498fb598dd
JoeNW <30888343+JoeNW@users.noreply.github.com>
2018-08-20 21:33:57 +0800

    Update client.py

--- client.py
+++ client.py
@@ -7,9 +7,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 data_to_sent = 'hello tcp socket'
 try:
-        sock.connect(('192.168.1.104', 80))
+        sock.connect(('192.168.1.104',80))
 
-        sent = sock.send(data_to_sent)
+        sent = sock.send(data_to_sent.encode())
         print(sock.recv(1024))
 finally:
         sock.close()
