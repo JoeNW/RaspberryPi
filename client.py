f10dbacd8871bceb87acbadee339969f92b53141
JoeNW <30888343+JoeNW@users.noreply.github.com>
2018-08-27 14:18:23 +0800

    client.py

--- client.py
+++ client.py
@@ -1,16 +1,15 @@
 #client
-
 import socket
 import sys
-   
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if __name__ == '__main__': #如果模块被直接运行，则代码块被运行，如果模块被导入，则代码块不被运行
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#创建TCP Socket
 
-data_to_sent = 'hello tcp socket'
-try:
-        sock.connect(('192.168.1.104',80))
+    data_to_sent = 'hello tcp socket'
+    try:
+        sock.connect(('192.168.1.104',9999))
 
         sent = sock.send(data_to_sent.encode())
         print(sock.recv(1024))
-finally:
+    finally:
         sock.close()
         print('socket closed')
